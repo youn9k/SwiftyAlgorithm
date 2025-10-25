cd77cd7c296b1cb716e744ce03d9af412f500230
Youngkyu Song <ace_lephant@naver.com>
2025-10-25 11:27:31 +0900

    [level 2] Title: 프로세스, Time: 1.58 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.swift
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -28,15 +28,15 @@ func solution(_ priorities:[Int], _ location:Int) -> Int {
     var queue = Queue()
     var count = 0
     
-    for (i, p) in priorities.enumerated() {
-        queue.push(Process(location: i, priority: p))
+    priorities.enumerated().forEach { 
+        queue.push(Process(location: $0.offset, priority: $0.element))
     }
     
     while !queue.queue.isEmpty {
         let current = queue.pop()!
         let isExist = queue.queue.contains { $0.priority > current.priority }
         if isExist {
-            queue.push(current) 
+            queue.push(current)
         } else {
             count += 1
             if current.location == location {
